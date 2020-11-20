b4cbbda07d9539fadb5396b60edb550206e99198
tkjoko09 <74136118+tkjoko09@users.noreply.github.com>
2020-11-20 08:03:18 +0700

    Game Perkalian
    Game Perkalian

---
+++ offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1605834113,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/papantulis-sheet0.png",
+		"images/pilihan-sheet0.png",
+		"images/kembali-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
